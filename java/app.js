deda3c04a65618110b18e558ce0aff8d97d54f5e
CarlLinusHedlund <carl.hedlund@stud.noroff.no>
2022-01-07 14:12:31 +0100

    done

---
+++ java/app.js
@@ -0,0 +1,39 @@
+//task 1
+var firstVar 
+
+//task 2
+
+var name = "Carl Linus";
+console.log (name)
+
+//task 3 
+
+var age = 25;
+console.log(age)
+
+
+//task 4 
+
+var division = 20 / 5; 
+console.log(division)
+
+//task 5
+
+var frog = "frog";
+typeof frog
+console.log (typeof(frog))
+
+
+//task 6 - 7
+
+var orderHasShipped =true; 
+if (orderHasShipped === true) {
+    alert ("Your order are on the way to you!")
+    console.log (orderHasShipped)
+} else {
+    alert ("your order is not shipped")
+};
+
+for (var count = 0; count <= 9; count++){
+    console.log(count);
+}
